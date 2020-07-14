3624f1ed6953e603ba0098b7d17e8b133b905952
Hubert Pol <hubertpol@wp.pl>
2020-07-14 23:18:46 +0200

    add props

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './App.scss';
 import List from '../List/List.js';
-
+import Hero from '../Hero/Hero.js';
 
 
 class App extends React.Component {
@@ -10,7 +10,9 @@ class App extends React.Component {
       <main className={styles.component}>
         <h1 className={styles.title}>My first React app</h1>
         <h2 className={styles.subtitle}>Hello world!</h2>
-        <List />
+        <List title={['Things to do ', <sup>soon!</sup>]}>
+        
+        </List>
       </main>
     )
   }
