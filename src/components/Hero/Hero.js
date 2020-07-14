bfdc85536ca9b4f200cd0a37d5e36d774b262d4a
Hubert Pol <hubertpol@wp.pl>
2020-07-14 21:09:39 +0200

    add components list and hero

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Hero.scss';
+
+
+const Hero = () => (
+    <header className={styles.component}>
+      <h2 className={styles.title}>eszlo laszlo</h2>
+      <img className={styles.image} src='http://uploads.kodilla.com/bootcamp/fer/11.react/space.png'/>
+    </header>
+  );
+
+  export default Hero;
