4b0fba090f6481a4e5397c507bd9e5540c74148f
Hubert Pol <hubertpol@wp.pl>
2020-07-15 22:22:17 +0200

    add cart and icon

---
+++ src/components/Card/Card.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './Card.scss';
+
+
+
+class Card extends React.Component {
+    
+      render() {
+        return (
+          <div className={styles.component}>
+              <h3></h3>
+          </div>
+        )
+      }
+    }
+    
+    export default Card;
+
