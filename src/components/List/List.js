2ae86e2cb7f3ca32c5e5c0669384b7cc7745efd3
Hubert Pol <hubertpol@wp.pl>
2020-07-15 20:39:21 +0200

    bug columns

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,17 +3,23 @@ import styles from './List.scss';
 import Hero from '../Hero/Hero';
 import PropTypes from 'prop-types';
 import Column from '../Column/Column';
+import {settings} from '../../data/dataStore';
+import ReactHtmlParser from 'react-html-parser';
 
 
 
 class List extends React.Component {
+  state = {
+    columns: this.props.columns || [],
+  }
     static propTypes = {
         title: PropTypes.node.isRequired,
-        children: PropTypes.node.isRequired,
+        description: PropTypes.node,
+        columns: PropTypes.array,
         image: PropTypes.string,
         }
     static defaultProps = {
-        children: <p>I can do all the things!!!</p>,
+        description: settings.defaultListDescription,
         }
 
       
@@ -23,13 +29,12 @@ class List extends React.Component {
         <Hero titleText={this.props.title} 
               image={this.props.image}
         />
-        <div className={styles.description}>
-            {this.props.children}
+        <div className={styles.description}>{ReactHtmlParser(this.props.description)}
         </div>
         <div className={styles.columns}>
-            <Column title= 'rambo'></Column>
-            <Column title='lucky'></Column>
-            <Column title='dog'></Column>
+            {this.state.columns.map(({key, ...columnProps}) => (
+              <Column key={key} {...columnProps} />
+            ))}
         </div>
       </section>
     )
